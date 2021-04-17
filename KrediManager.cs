d71cb625760e76e8232332949552be523018a0df
ElanurAltintop <77887554+ElanurAltintop@users.noreply.github.com>
2021-04-17 17:02:06 +0300

    Add files via upload
    object oriented programming features

---
+++ KrediManager.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP3
+{
+    interface IKrediManager
+    {
+      //Birbirinin alternatifi olan ama kod içerikleri farklı olan kodlar için kullanılır.
+        void Hesapla();  
+        void BiseyYap();
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace OOP3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            IKrediManager ıhtiyacKrediManager = new IhtiyacKrediManager();
+           // ıhtiyacKrediManager.Hesapla();
+
+            IKrediManager tasitKrediManager = new TasitKrediManager();
+           // tasitKrediManager.Hesapla();
+
+            IKrediManager konutKrediManager = new KonutKrediManager();
+            // konutKrediManager.Hesapla();
+
+            ILoggerService databaseloggerService = new DatabaseLoggerService();
+            ILoggerService fileloggerService = new FileLoggerService();
+
+            BasvuruManager basvuruManager = new BasvuruManager();
+            basvuruManager.BasvuruYap(konutKrediManager,databaseloggerService);
+            basvuruManager.BasvuruYap(tasitKrediManager, fileloggerService);
+
+            List<IKrediManager> krediler = new List<IKrediManager>() {ıhtiyacKrediManager, tasitKrediManager, konutKrediManager };
+            
+            //basvuruManager.KrediOnBilgilendirmesiYap(krediler); 
+        }
+    }
+}
